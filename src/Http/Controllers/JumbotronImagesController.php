c65c65d8e904d90dcee43156b2173425eecd84b9
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-25 17:37:40 +0200

    adding controllers!

---
+++ src/Http/Controllers/JumbotronImagesController.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace DavideCasiraghi\LaravelJumbotronImages\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use DavideCasiraghi\LaravelJumbotronImages\Models\JumbotronImage;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use DavideCasiraghi\LaravelJumbotronImages\Facades\PhpResponsiveQuote;
+
+class JumbotronImagesController
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $searchKeywords = $request->input('keywords');
+        //$searchCategory = $request->input('category_id');
+        $countriesAvailableForTranslations = LaravelLocalization::getSupportedLocales();
+
+        if ($searchKeywords) {
+            $quotes = Quote::orderBy('author')
+                                     ->where('author', 'like', '%'.$request->input('keywords').'%')
+                                     ->paginate(20);
+        } else {
+            $quotes = Quote::orderBy('author')
+                                     ->paginate(20);
+        }
+
+        return view('php-responsive-quote::quotes.index', compact('quotes'))
+                             ->with('i', (request()->input('page', 1) - 1) * 20)
+                             ->with('searchKeywords', $searchKeywords)
+                             ->with('countriesAvailableForTranslations', $countriesAvailableForTranslations);
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('php-responsive-quote::quotes.create');
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $quote = new Quote();
+        $quote->author = $request->get('author');
+        $quote->text = $request->get('text');
+
+        // Set the default language to edit the quote in English
+        App::setLocale('en');
+
+        $this->saveOnDb($request, $quote);
+
+        return redirect()->route('php-responsive-quote.index')
+                            ->with('success', 'Quote added succesfully');
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $quoteId
+     * @return \Illuminate\Http\Response
+     */
+    public function show($quoteId = null)
+    {
+        $quote = Quote::find($quoteId);
+
+        return view('php-responsive-quote::quotes.show', compact('quote'));
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $quoteId
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($quoteId = null)
+    {
+        $quote = Quote::find($quoteId);
+
+        return view('php-responsive-quote::quotes.edit', compact('quote'));
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $quoteId
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $quoteId)
+    {
+        $quote = Quote::find($quoteId);
+
+        // Set the default language to update the quote in English
+        App::setLocale('en');
+
+        $this->saveOnDb($request, $quote);
+
+        return redirect()->route('php-responsive-quote.index')
+                            ->with('success', 'Quote updated succesfully');
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $quoteId
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($quoteId)
+    {
+        $quote = Quote::find($quoteId);
+        $quote->delete();
+
+        return redirect()->route('php-responsive-quote.index')
+                            ->with('success', 'Quote deleted succesfully');
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Save the record on DB.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \DavideCasiraghi\PhpResponsiveRandomQuote\Models\Quote  $quote
+     * @return void
+     */
+    public function saveOnDb($request, $quote)
+    {
+        $quote->translateOrNew('en')->text = $request->get('text');
+        $quote->author = $request->get('author');
+        $quote->save();
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRandomQuote()
+    {
+        $quote = PhpResponsiveQuote::getRandomQuote();
+
+        // the view name is set in the - Service provider - boot - loadViewsFrom
+        return view('php-responsive-quote::show-random-quote', [
+            'quoteAuthor' => $quote['author'],
+            'quoteText' => $quote['text'],
+        ]);
+    }
+}
---
+++ src/Http/Controllers/JumbotronImagesTranslationController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace DavideCasiraghi\LaravelJumbotronImages\Http\Controllers;
+
+use Validator;
+use Illuminate\Http\Request;
+use DavideCasiraghi\LaravelJumbotronImages\Models\JumbotronImage;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use DavideCasiraghi\LaravelJumbotronImages\Models\JumbotronImageTranslation;
+
+class JumbotronImagesTranslationController
+{
+    /***************************************************************************/
+
+    /**
+     * Show the form for creating a new resource.
+     * @param int $quoteId
+     * @param string $languageCode
+     * @return \Illuminate\Http\Response
+     */
+    public function create($quoteId, $languageCode)
+    {
+        $selectedLocaleName = $this->getSelectedLocaleName($languageCode);
+
+        return view('php-responsive-quote::quoteTranslations.create')
+                ->with('quoteId', $quoteId)
+                ->with('languageCode', $languageCode)
+                ->with('selectedLocaleName', $selectedLocaleName);
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $quoteId
+     * @param string $languageCode
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($quoteId, $languageCode)
+    {
+        $quoteTranslation = QuoteTranslation::where('quote_id', $quoteId)
+                        ->where('locale', $languageCode)
+                        ->first();
+
+        $selectedLocaleName = $this->getSelectedLocaleName($languageCode);
+
+        return view('php-responsive-quote::quoteTranslations.edit', compact('quoteTranslation'))
+                    ->with('quoteId', $quoteId)
+                    ->with('languageCode', $languageCode)
+                    ->with('selectedLocaleName', $selectedLocaleName);
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        // Validate form datas
+        $validator = Validator::make($request->all(), [
+                'text' => 'required',
+            ]);
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $quoteTranslation = new QuoteTranslation();
+
+        $this->saveOnDb($request, $quoteTranslation);
+
+        return redirect()->route('php-responsive-quote.index')
+                            ->with('success', 'Quote translation added succesfully');
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $quoteTranslationId
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $quoteTranslationId)
+    {
+        request()->validate([
+            'text' => 'required',
+        ]);
+
+        $quoteTranslation = QuoteTranslation::find($quoteTranslationId);
+
+        $this->saveOnDb($request, $quoteTranslation);
+
+        return redirect()->route('php-responsive-quote.index')
+                            ->with('success', 'Quote translation added succesfully');
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Save the record on DB.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \DavideCasiraghi\PhpResponsiveRandomQuote\Models\QuoteTranslation  $quoteTranslation
+     * @return void
+     */
+    public function saveOnDb($request, $quoteTranslation)
+    {
+        $quoteTranslation->quote_id = $request->get('quote_id');
+        $quoteTranslation->locale = $request->get('language_code');
+
+        $quoteTranslation->text = $request->get('text');
+        $quoteTranslation->save();
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Get the language name from language code.
+     *
+     * @param  string $languageCode
+     * @return string
+     */
+    public function getSelectedLocaleName($languageCode)
+    {
+        $countriesAvailableForTranslations = LaravelLocalization::getSupportedLocales();
+        $ret = $countriesAvailableForTranslations[$languageCode]['name'];
+
+        return $ret;
+    }
+
+    /***************************************************************************/
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $quoteTranslationId
+     */
+    public function destroy($quoteTranslationId)
+    {
+        $quoteTranslation = QuoteTranslation::find($quoteTranslationId);
+        $quoteTranslation->delete();
+
+        return redirect()->route('php-responsive-quote.index')
+                            ->with('success', 'Quote translation deleted succesfully');
+    }
+}
