0f676ea335c2d9c82a5da23753ea1c60cd4577a5
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-26 09:51:34 +0200

    finishing the jumbotron show view

--- resources/views/jumbotronImages/show.blade.php
+++ resources/views/jumbotronImages/show.blade.php
@@ -1,9 +1,16 @@
 @extends('laravel-jumbotron-images::jumbotronImages.layout')
 
 @section('content')
-    {{$jumbotronImage->title}}
-    {{$jumbotronImage->body}}
-    {{$jumbotronImage->button_text}}
-    {{$jumbotronImage->image_file_name}}
-    {{$jumbotronImage->button_url}}
+    
+    @if($jumbotronImage)
+        {{$jumbotronImage->title}}
+        {{$jumbotronImage->body}}
+        {{$jumbotronImage->button_text}}
+        {{$jumbotronImage->image_file_name}}
+        {{$jumbotronImage->button_url}}
+    @else
+        <div class="alert alert-warning" role="alert">
+            No jumbotron corresponding to the specified ID has been found.
+        </div>
+    @endif
 @endsection
