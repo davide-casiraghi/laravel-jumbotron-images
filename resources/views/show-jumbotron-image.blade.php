8a1f33ce42f463040aff4896cf230bc6a8e3d97a
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-26 19:59:28 +0200

    added getJumbotronHeightArray function

--- resources/views/show-jumbotron-image.blade.php
+++ resources/views/show-jumbotron-image.blade.php
@@ -16,17 +16,17 @@
 </div>
 
 
-{{--
+
 <div class="jumbotronImage">
     <section class="hero {{$parameters['white_moon']}} {{$parameters['banner_height']}} {{$parameters['parallax']}} {{$parameters['banner_height']}}" style="{{$parameters['image']}} {{$parameters['background_color']}} {{$parameters['parallax']}}" {{$parameters['parallax_ratio']}}>
         <div class="hero-body" style="{{$parameters['vertical_text_alignment']}}">
             
             <div class="container" style="{{$parameters['horizontal_text_alignment']}} {{$parameters['text_shadow']}}">
-                <h1 class="title" style="{{$parameters['title_font_size']}} {{$parameters['title_font_weight']}} {{$parameters['title_font_family']}}">
+                <h1 class="title">
                     <p style="{{$parameters['text_width']}}">{{$parameters['title']}}</p>
                 </h1>
-                <h2 class="subtitle" style="{{$parameters['description_font_size']}} {{$parameters['description_font_weight']}}">
-                    <p style="{{$parameters['text_width']}} {{$parameters['description_font_family']}}">{{$parameters['description']}}</p>
+                <h2 class="subtitle">
+                    <p style="{{$parameters['text_width']}}">{{$parameters['description']}}</p>
                 </h2>
 
                 {{$parameters['button']}}
@@ -43,4 +43,3 @@
         
     
 </div>
---}}
