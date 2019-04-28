b6b77c3c683c95480614e855f5651412f0d70bdd
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-04-28 15:24:57 +0200

    refactored code

--- src/LaravelJumbotronImages.php
+++ src/LaravelJumbotronImages.php
@@ -49,6 +49,7 @@ class LaravelJumbotronImages
              'cover_opacity' => 'opacity: '.$jumbotronImage->cover_opacity.';',
              'background_color' => 'background: #'.$jumbotronImage->background_color.';',
              'image' => 'background-image:url(/storage/images/jumbotron_images/'.$jumbotronImage->image_file_name.');',
+             'text_horizontal_alignment' => 'text-align: '.$jumbotronImage->text_horizontal_alignment.';',
          ];
          $ret['white_moon'] = ($jumbotronImage->white_moon == 1) ? " moon-curve " : "";
          $ret['scroll_down_arrow'] = ($jumbotronImage->scroll_down_arrow == 1) ? "<div class='scroll-arrow white'><span>SCROLL DOWN</span><img src='/vendor/laravel-jumbotron-images/assets/images/angle-down-regular.svg'></div>" : "";
@@ -56,20 +57,6 @@ class LaravelJumbotronImages
         /* Parallax - The element is defined with stellar plugin like: <section class="parallax" data-stellar-background-ratio="0.5" ><span>Summer</span></section>*/
         $ret['parallax'] = ($jumbotronImage->parallax == 1) ? " parallax" : "";
         $ret['parallax_ratio'] = ($jumbotronImage->parallax == 1) ? "data-stellar-background-ratio='0.5'" : "";
-
-        /* Horizontal Alignment */
-        $ret['text_horizontal_alignment'] = "text-align: ".$jumbotronImage->text_horizontal_alignment.";";
-        /*switch ($jumbotronImage->text_horizontal_alignment) {
-            case 0:
-                $ret['text_horizontal_alignment'] = 'text-align: left;';
-                break;
-            case 1:
-                $ret['text_horizontal_alignment'] = 'text-align: center;';
-                break;
-            case 2:
-                $ret['text_horizontal_alignment'] = 'text-align: right;';
-                break;
-        }*/
 
         /* Text Width */
         if ($jumbotronImage->text_width != 100) {
